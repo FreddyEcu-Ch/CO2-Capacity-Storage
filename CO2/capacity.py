a187059c0f87433451093be8e603885ff06d3ff0
Fabian <cafapazm@espol.edu.ec>
2022-12-15 14:03:25 -0500

    adelanto1

---
+++ CO2/capacity.py
@@ -0,0 +1,17 @@
+# Import Python Libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import ticker
+from matplotlib.ticker import AutoMinorLocator
+from streamlit_option_menu import option_menu
+from scipy.stats import norm
+with st.sidebar:
+    options = option_menu(menu_title="main menu",options=["Home", "Reservoir data"],icons=[])
+#Distribución normal Bo
+BO_norm = norm.rvs(loc=1.5, scale=0.5, size=100)
+BO_norm = np.where(BO_norm < 1, 1, BO_norm)
+BO_norm = np.where(BO_norm > 2, 2, BO_norm)
