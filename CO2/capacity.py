fb8110cffc863b872b185952d171c18d3f1dc648
fredd <freddypcarrion@gmail.com>
2022-12-30 11:39:28 -0500

    code formatted with blackd.

--- CO2/capacity.py
+++ CO2/capacity.py
@@ -5,30 +5,59 @@ from PIL import Image
 from funciones import bachu, bachuprob
 from sqlalchemy import create_engine
 import pandas as pd
-#Insertar un icono
+
+# Insertar un icono
 icon = Image.open("Resourse/logo.jpg")
-#Desarrollo de la interfaz
-st.set_page_config(page_title=" App Capacidad de almacenamiento Geológico", page_icon = icon)
-st.title('Geological CO2 Storage App')
-st.write("This application aims to calculate the geological storage capacity for oil reservoir using probabilistic methods and petrophysical input variables.")
+# Desarrollo de la interfaz
+st.set_page_config(
+    page_title=" App Capacidad de almacenamiento Geológico", page_icon=icon
+)
+st.title("Geological CO2 Storage App")
+st.write(
+    "This application aims to calculate the geological storage capacity for oil reservoir using probabilistic methods"
+    " and petrophysical input variables."
+)
 with st.sidebar:
-    options = option_menu("Main menu", options=["Home", "Reservoir data"], icons=["house", "gear"], menu_icon="cast")
+    options = option_menu(
+        "Main menu",
+        options=["Home", "Reservoir data"],
+        icons=["house", "gear"],
+        menu_icon="cast",
+    )
     options
 image = Image.open("Resourse/ccus.jpg")
 st.image(image, width=100, use_column_width=True)
 if options == "Home":
-    st.markdown("""La ubicación de formaciones geológicas potencialmente efectivas para el almacenamiento geológico y la posterior evaluación de la capacidad regional de almacenamiento de CO2 en dichas formaciones son requisitos básicos para definir y predecir estrategias de entrada hacia la transición energética a nivel local, nacional o internacional. Sin duda, dicha evaluación será de gran interés para aquellos involucrados en actividades directamente relacionadas con el almacenamiento desde una perspectiva operativa especializada, y para los responsables del desarrollo de políticas y planes de acción, o quizás para las autoridades reguladoras.""")
-    st.markdown("""Una de las tecnologias para reducir las emisiones es la captura, uso y almacenamiento de CO2 por su nombre en ingles CCUS, esta es una de las tecnologìas màs prometedoras para reducir el impacto de los gases de efecto invernadero sobre el planeta """)
+    st.markdown(
+        """La ubicación de formaciones geológicas potencialmente efectivas para el almacenamiento geológico y la
+         posterior evaluación de la capacidad regional de almacenamiento de CO2 en dichas formaciones son requisitos
+          básicos para definir y predecir estrategias de entrada hacia la transición energética a nivel local, nacional
+           o internacional. Sin duda, dicha evaluación será de gran interés para aquellos involucrados en actividades
+            directamente relacionadas con el almacenamiento desde una perspectiva operativa especializada, y para los
+             responsables del desarrollo de políticas y planes de acción, o quizás para las autoridades reguladoras."""
+    )
+    st.markdown(
+        """Una de las tecnologías para reducir las emisiones es la captura, uso y almacenamiento de CO2 por su nombre
+         en ingles CCUS, esta es una de las tecnologías màs prometedoras para reducir el impacto de los gases de efecto
+          invernadero sobre el planeta """
+    )
     # OBJETIVO GENERAL
     st.header("""**Objetivo:** """)
-    st.markdown("""Diseñar un aplicativo web que permita calcular el almacenamiento de CO2 en yacimientos de petróleo y gas, utilizando mètodos probabilìsticos y deterministicos""")
+    st.markdown(
+        """Diseñar un aplicativo web que permita calcular el almacenamiento de CO2 en yacimientos de petróleo y gas,
+         utilizando métodos probabilísticos y determinísticos"""
+    )
 
 elif options == "Reservoir data":
     with st.sidebar:
-        options = option_menu("Select Methodology", options=["Deterministic", "Probabilistic"], icons=["calculator", "calculator"])
+        options = option_menu(
+            "Select Methodology",
+            options=["Deterministic", "Probabilistic"],
+            icons=["calculator", "calculator"],
+        )
     if options == "Deterministic":
-        st.subheader("**Mètodo de Bachu determinìstico**")
-        st.subheader("Ingrese los valores petrofìsicos de su reservorio:")
+        st.subheader("**Método de Bachu determinístico**")
+        st.subheader("Ingrese los valores petrofísicos de su reservorio:")
         densidad = st.number_input("Valor de densidad de CO2 en Kg/m3 :")
         Rf = st.number_input("Valor de Rf en tanto por 1 :")
         OOIP = st.number_input("Valor de OOIP en m3 :")
@@ -38,9 +67,9 @@ elif options == "Reservoir data":
         st.success(c)
     elif options == "Probabilistic":
         st.subheader("**Seleccione el método:**")
-        metodo = st.selectbox("Método", ("Bachu probabilístico","Zhong y Carr"))
+        metodo = st.selectbox("Método", ("Bachu probabilístico", "Zhong y Carr"))
         if metodo == "Bachu probabilístico":
-            st.subheader("**Mètodo de Bachu probabilístico**")
+            st.subheader("**Método de Bachu probabilístico**")
             upload_file = st.file_uploader("Sube tu documento db:")
             engine = create_engine("sqlite:///CO2/CO2_EOR1.db")
             df = pd.read_sql_query("SELECT * FROM Datos", engine)
@@ -51,6 +80,4 @@ elif options == "Reservoir data":
             df2
         elif metodo == "Zhong y Carr":
             st.subheader("**Método de Zhong y Carr**")
-            #upload_file = st.sidebar.file_uploader("Sube tu documento csv:")
-
-
+            # upload_file = st.sidebar.file_uploader("Sube tu documento csv:")
